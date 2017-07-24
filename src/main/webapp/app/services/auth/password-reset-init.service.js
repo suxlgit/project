fda2c5cf77ecf24e2506c4bfeee8507f7e6baa76
suxl <449044976@qq.com>
2017-07-24 14:35:40 +0800

    first commit

---
+++ src/main/webapp/app/services/auth/password-reset-init.service.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('helloApp')
+        .factory('PasswordResetInit', PasswordResetInit);
+
+    PasswordResetInit.$inject = ['$resource'];
+
+    function PasswordResetInit($resource) {
+        var service = $resource('api/account/reset_password/init', {}, {});
+
+        return service;
+    }
+})();
